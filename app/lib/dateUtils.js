f54a32dd3cd62115ce0d97db0ba5c8f18a81b5a0
Shaswat-31 <143761992+Shaswat-31@users.noreply.github.com>
2024-09-05 01:01:43 +0530

    Revert "timezone added"
    This reverts commit 0b233ff1fb4421410105c5785ac747c49830e4de.

--- app/lib/dateUtils.js
+++ app/lib/dateUtils.js
@@ -1,37 +1,32 @@
 // dateUtils.js
 
-const convertToLocalTime = (date, timeZone = 'Asia/Kolkata') => {
-  return new Date(date.toLocaleString('en-US', { timeZone }));
-};
-
 export const getStartOfWeek = () => {
-  const now = convertToLocalTime(new Date());
-  const startOfWeek = now.getDate() - now.getDay(); // Get the start of the week
-  return new Date(now.setDate(startOfWeek));
-};
-
-export const getEndOfWeek = () => {
-  const now = convertToLocalTime(new Date());
-  const endOfWeek = now.getDate() - now.getDay() + 6; // Get the end of the week
-  return new Date(now.setDate(endOfWeek));
-};
-
-export const getStartOfDay = () => {
-  const now = convertToLocalTime(new Date());
-  return new Date(now.setHours(0, 0, 0, 0)); // Start of today
-};
-
-export const getEndOfDay = () => {
-  const now = convertToLocalTime(new Date());
-  return new Date(now.setHours(23, 59, 59, 999)); // End of today
-};
-
-export const getStartOfMonth = () => {
-  const now = convertToLocalTime(new Date());
-  return new Date(now.getFullYear(), now.getMonth(), 1);
-};
-
-export const getEndOfMonth = () => {
-  const now = convertToLocalTime(new Date());
-  return new Date(now.getFullYear(), now.getMonth() + 1, 0);
-};
+    const now = new Date();
+    const startOfWeek = now.getDate() - now.getDay(); // Get the start of the week
+    return new Date(now.setDate(startOfWeek));
+  };
+  
+  export const getEndOfWeek = () => {
+    const now = new Date();
+    const endOfWeek = now.getDate() - now.getDay() + 6; // Get the end of the week
+    return new Date(now.setDate(endOfWeek));
+  };
+  
+  export const getStartOfDay = () => {
+    const now = new Date();
+    return new Date(now.setHours(0, 0, 0, 0)); // Start of today
+  };
+  
+  export const getEndOfDay = () => {
+    const now = new Date();
+    return new Date(now.setHours(23, 59, 59, 999)); // End of today
+  };
+  export const getStartOfMonth = () => {
+    const now = new Date();
+    return new Date(now.getFullYear(), now.getMonth(), 1);
+  };
+  
+  export const getEndOfMonth = () => {
+    const now = new Date();
+    return new Date(now.getFullYear(), now.getMonth() + 1, 0);
+  };
